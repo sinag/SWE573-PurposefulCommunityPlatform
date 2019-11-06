1e211a5e642535f305f1b933e0a3e33d1c1e4d18
sinag <sinagulsen@gmail.com>
2019-11-06 13:58:33 +0300

    closes #54

--- code/community/templatetags/community_tags.py
+++ code/community/templatetags/community_tags.py
@@ -10,5 +10,10 @@ def subscription_id(community_id, user_id):
 
 
 @register.filter
-def subscription_count(community_id, user_id):
+def subscription_count_by_user(community_id, user_id):
     return Community.objects.get(id=community_id).subscription_set.all().filter(user__id=user_id).count()
+
+
+@register.filter
+def subscription_count(community_id):
+    return Community.objects.get(id=community_id).subscription_set.all().count()
