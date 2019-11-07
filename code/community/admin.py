fc27b59e40640acda0b47cf157aa3e1ce7f7aa63
sinag <sinagulsen@gmail.com>
2019-11-07 16:35:28 +0300

    closes #60, closes #65, closes #66, closes #68, closes #69

--- code/community/admin.py
+++ code/community/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Community
+from .models import Community, DataType, Property, Instance, TextField, IntegerField
 
 
 class CommunityAdmin(admin.ModelAdmin):
@@ -13,4 +13,60 @@ class CommunityAdmin(admin.ModelAdmin):
     readonly_fields = ['created_on']
 
 
+class TextFieldAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['instance', 'property', 'value']}),
+    ]
+    list_display = ('id', 'instance', 'property', 'value')
+    list_filter = ['instance', 'property']
+    search_fields = ['instance', 'property']
+
+
+class IntegerFieldAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['instance', 'property', 'value']}),
+    ]
+    list_display = ('id', 'instance', 'property', 'value')
+    list_filter = ['instance', 'property']
+    search_fields = ['instance', 'property']
+
+
+class DataTypeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'description', 'author', 'generic']}),
+        ('Date information', {'fields': ['created_on'], 'classes': ['collapse']}),
+    ]
+    list_display = ('id', 'name', 'created_on', 'author', 'generic')
+    list_filter = ['author', 'generic']
+    search_fields = ['name', 'author', 'generic']
+    readonly_fields = ['created_on']
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'type', 'datatype']}),
+        ('Date information', {'fields': ['created_on'], 'classes': ['collapse']}),
+    ]
+    list_display = ('id', 'name', 'created_on', 'type', 'datatype')
+    list_filter = ['datatype', 'type']
+    search_fields = ['name', 'type', 'datatype']
+    readonly_fields = ['created_on']
+
+
+class InstanceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['author', 'datatype']}),
+        ('Date information', {'fields': ['created_on', 'updated_on'], 'classes': ['collapse']}),
+    ]
+    list_display = ('id', 'author', 'created_on', 'updated_on', 'datatype')
+    list_filter = ['datatype', 'author']
+    search_fields = ['datatype', 'author']
+    readonly_fields = ['created_on', 'updated_on']
+
+
+admin.site.register(TextField, TextFieldAdmin)
+admin.site.register(IntegerField, IntegerFieldAdmin)
+admin.site.register(Instance, InstanceAdmin)
+admin.site.register(Property, PropertyAdmin)
 admin.site.register(Community, CommunityAdmin)
+admin.site.register(DataType, DataTypeAdmin)
--- code/community/models.py
+++ code/community/models.py
@@ -4,13 +4,87 @@ from django.db import models
 from root import settings
 
 
-class Community(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=settings.DEFAULT_ADMIN)
+class DataType(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=settings.DEFAULT_ADMIN,
+                               blank=False, null=False, db_index=True)
     name = models.CharField(max_length=100)
-    created_on = models.DateTimeField(auto_now_add=True, blank=False)
-    description = models.CharField(max_length=500, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    description = models.CharField(max_length=500, blank=False, null=False)
+    generic = models.BooleanField(db_index=True)  # False = Custom, True = Generic
+
+    def __str__(self):
+        return str(str(self.id) + '-' + self.name)
+
+    class Meta:
+        verbose_name = "datatype"
+        verbose_name_plural = "datatypes"
+
+
+class Instance(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=settings.DEFAULT_ADMIN,
+                               blank=False, null=False, db_index=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    updated_on = models.DateTimeField(auto_now_add=True, blank=False,
+                                      null=False)  # ToDo - Auto update this on record update
+    datatype = models.ForeignKey(DataType, on_delete=models.PROTECT, blank=False, null=False, db_index=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "instance"
+        verbose_name_plural = "instances"
+
+
+class Property(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    created_on = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    type = models.SmallIntegerField(blank=False, null=False,
+                                    db_index=True)  # 0 = Text, 1 = Integer, 2 = Datetime, 3 = Enumeration
+    datatype = models.ForeignKey(DataType, on_delete=models.PROTECT, blank=False, null=False, db_index=True)
+
+    def __str__(self):
+        return str(str(self.id) + '-' + self.name)
+
+    class Meta:
+        verbose_name = "property"
+        verbose_name_plural = "properties"
+
+
+class TextField(models.Model):
+    value = models.CharField(max_length=255)
+    instance = models.ForeignKey(Instance, on_delete=models.PROTECT, blank=False, null=False, db_index=True)
+    property = models.ForeignKey(Property, on_delete=models.PROTECT, blank=False, null=False, db_index=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "textfield"
+        verbose_name_plural = "textfields"
+
+
+class IntegerField(models.Model):
+    value = models.BigIntegerField()
+    instance = models.ForeignKey(Instance, on_delete=models.PROTECT, blank=False, null=False, db_index=True)
+    property = models.ForeignKey(Property, on_delete=models.PROTECT, blank=False, null=False, db_index=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "integerfield"
+        verbose_name_plural = "integerfields"
+
+
+class Community(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=settings.DEFAULT_ADMIN,
+                               blank=False, null=False, db_index=True)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    created_on = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    description = models.CharField(max_length=500, blank=False, null=False)
     # ToDo convert post_count to joined table result when posts are available
-    post_count = models.BigIntegerField(blank=False, db_index=True)
+    post_count = models.BigIntegerField(blank=False, null=False, db_index=True)
 
     def __str__(self):
         return str(self.name)
