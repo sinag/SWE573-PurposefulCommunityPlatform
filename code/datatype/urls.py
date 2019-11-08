7755f0bfc40dcbb5b7309ee1cf15cf8e062938ae
sinag <sinagulsen@gmail.com>
2019-11-08 11:34:49 +0300

    partial commit for #79

---
+++ code/datatype/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from datatype.views import CreateView
+from . import views
+
+app_name = 'datatype'
+urlpatterns = [
+    # path('create/<int:community_id>', CreateView.as_view(), name='create'),
+    path('<int:community_id>', views.IndexView.as_view(), name='index'),
+]
--- code/datatype/views.py
+++ code/datatype/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
+from django.views.generic import CreateView
+from .models import DataType
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'datatype/index.html'
+    context_object_name = 'datatypes'
+
+    def get_queryset(self):
+        # user = self.request.user.id
+        """
+        Return top communities by post count
+        """
+        return DataType.objects.all().filter(community__id=self.kwargs.get('community_id'))
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        # context['subscriptions'] = Hotel.objects.all().order_by('star').reverse()[:3]
+        # # Add any other variables to the context here
+        return context
+
+# class CreateView(CreateView):
+#     model = DataType
+#     fields = ['name', 'description', 'post_count']
+#     template_name = 'datatype/create.html'
+#
+#     def form_valid(self, form):
+#         form.instance.author = self.request.user
+#         return super().form_valid(form)
+#
+#     def get_success_url(self):
+#         return reverse('datatype:index')  # Todo - this will go to datatypes form
